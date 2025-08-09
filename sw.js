6690e341ab68441346205b034d48349d6ba07329
jrshultz <jrshultz@gmail.com>
2025-08-09 10:35:23 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('wp-stats-cache-v7').then(c=>c.addAll([
+  e.waitUntil(caches.open('wp-stats-cache-v8').then(c=>c.addAll([
     './','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'
   ])));
 });
