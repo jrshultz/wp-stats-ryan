10da75c7bc07d4d0838717fabf4705b48268e635
jrshultz <jrshultz@gmail.com>
2025-08-09 14:04:01 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,4 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('wp-v15').then(c=>c.addAll(['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'])))});
+const CACHE='wp-v16-full-debug';
+self.addEventListener('install',e=>{self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'])))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
